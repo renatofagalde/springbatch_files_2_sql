3dd406930f3de100eda86d417dc2d9c428195fc3
renato <renato@likwi.com.br>
2022-11-02 14:26:27 -0300

    Initial commit

---
+++ src/main/java/br/com/likwi/springbatch_files_2_sql/SpringbatchFiles2SqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.likwi.springbatch_files_2_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchFiles2SqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchFiles2SqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/likwi/springbatch_files_2_sql/SpringbatchFiles2SqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.likwi.springbatch_files_2_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchFiles2SqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
